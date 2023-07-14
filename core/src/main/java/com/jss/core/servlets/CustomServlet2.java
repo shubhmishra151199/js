bd7777cdca3170f13c9bc8256fe225105b875b6d
shubhmishra151199 <139145082+shubhmishra151199@users.noreply.github.com>
2023-07-14 13:04:38 +0530

    Create CustomServlet2.java

---
+++ core/src/main/java/com/jss/core/servlets/CustomServlet2.java
@@ -0,0 +1,28 @@
+package com.jss.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component(service = Servlet.class, property = {
+        "sling.servlet.paths=/bin/custom2",
+        "sling.servlet.methods=GET"
+})
+public class CustomServlet2 extends SlingSafeMethodsServlet {
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h1>Hello, S!</hi... how are you>");
+        writer.println("</body></html>");
+    }
+}
